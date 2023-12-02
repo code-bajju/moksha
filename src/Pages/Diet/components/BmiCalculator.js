313175553d2b2c5c9c07f5ca269386caec7cf069
code-bajju <11212766@mmec.mmdu.org>
2023-12-02 23:12:27 +0530

    chatbot and bmi update

--- src/Pages/Diet/components/BmiCalculator.js
+++ src/Pages/Diet/components/BmiCalculator.js
@@ -4,6 +4,7 @@ import { CalcButton } from '../styles/Buttons';
 import male from '../assets/male.png';
 import female from '../assets/female.png';
 import './base/BmiCalculation.css'
+// ... (your existing code)
 
 function BmiCalculator() {
   const { setName, setWeight, setHeight, calculate } = useContext(MainContext);
@@ -11,24 +12,37 @@ function BmiCalculator() {
 
   const imgPath = value === '1' ? male : female;
 
+  const isDetailsFilled = (name, weight, height) => {
+    return name.trim() !== '' && weight.trim() !== '' && height.trim() !== '';
+  };
+
   return (
     <div className='main-container'>
       <img className='bmi-image' src="https://github.com/code-bajju/Project-Image/blob/main/bmi.png?raw=true" alt='' />
       <div className='details'>
         <input type="text" placeholder='Your name...' onChange={(e) => setName(e.target.value)} />
         <input type="text" placeholder='Weight, etc 60kg' onChange={(e) => setWeight(e.target.value)} />
-        <input type="text" placeholder='Height, etc 160cm' onChange={(e) => setHeight(e.target.value / 100)} />
+        <input type="text" placeholder='Height, etc 160cm' onChange={(e) => setHeight(e.target.value / 100)}/>
         <div>
           <label>
             <input type="radio" value='1' checked={value === '1'} onChange={(e) => setValue(e.target.value)} />
-         <span>Male</span>
+            <span>Male</span>
           </label>
           <label>
             <input type="radio" value='2' checked={value === '2'} onChange={(e) => setValue(e.target.value)} />
             <span>Female</span>
           </label>
         </div>
-        <CalcButton type='button' className='btn' color={value} to='/results' onClick={calculate}>Calculate</CalcButton>
+        <CalcButton
+          type='button'
+          className='btn'
+          color={value}
+          to='/results'
+          onClick={isDetailsFilled ? calculate : undefined}
+          disabled={!isDetailsFilled}
+        >
+          Calculate
+        </CalcButton>
       </div>
     </div>
   );
